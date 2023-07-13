286182e2677ae35855df213779792b38fea35d2b
Dragos Neacsu <dragos.neacsu@yopeso.com>
2023-07-13 19:52:29 +0300

    Initial Commit

---
+++ SleepCalculator/SleepCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepCalculatorApp.swift
+//  SleepCalculator
+//
+//  Created by Dragos Neacsu on 13.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SleepCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
